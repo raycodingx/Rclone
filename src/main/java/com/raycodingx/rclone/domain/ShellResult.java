f95e0dfa07a492583f069aa59cd3cf4e37c5edef
raycodingx <raycodingx@gmail.com>
2023-11-09 00:19:48 +0800

    commandline result domain

---
+++ src/main/java/com/raycodingx/rclone/domain/ShellResult.java
@@ -0,0 +1,67 @@
+package com.raycodingx.rclone.domain;
+
+public class ShellResult {
+    private Boolean success;
+    private String cmd;
+
+    private String code;
+
+    private String msg;
+
+    public ShellResult() {
+    }
+
+    public ShellResult(Boolean success, String cmd) {
+        this.success = success;
+        this.cmd = cmd;
+    }
+
+    public ShellResult(Boolean success, String cmd, String code, String msg) {
+        this.success = success;
+        this.cmd = cmd;
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    @Override
+    public String toString() {
+        return "ShellResult{" +
+                "success=" + success +
+                ", cmd='" + cmd + '\'' +
+                ", code='" + code + '\'' +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
