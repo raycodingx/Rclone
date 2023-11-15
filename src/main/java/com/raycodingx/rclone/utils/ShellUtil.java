9cb2e6dd10eeaa100f9cdc5fc821de6d48ddf756
raycodingx <291633712@qq.com>
2023-11-15 13:46:20 +0800

    ShellUtil更改工具shell执行方法

--- src/main/java/com/raycodingx/rclone/utils/ShellUtil.java
+++ src/main/java/com/raycodingx/rclone/utils/ShellUtil.java
@@ -5,15 +5,20 @@ import com.raycodingx.rclone.domain.ShellResult;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class ShellUtil {
     public static ShellResult exec (String cmd, String pwd){
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("bash");
+        cmds.add("-c");
         if (pwd ==null | "".equals(pwd)){
             //
         }else {
             cmd = "echo " +pwd + " |sudo -S " + cmd;
         }
-        ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+        cmds.add(cmd);
+        ProcessBuilder processBuilder = new ProcessBuilder(cmds);
         try {
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
